1612b61529015563a07aa81e80bf2b85e287276f
nverno <noah.v.peart@gmail.com>
2018-10-23 21:11:35 -0400

    update iteration graph

--- src/slides.py
+++ src/slides.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-Some code to generate figures for slides.
+Generate some figures for slides.
 """
 
 ## Basic packages
@@ -22,8 +22,7 @@ import graphviz
 
 ## sklearn for model selection 
 from sklearn.model_selection import train_test_split, learning_curve
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix
 from sklearn import model_selection
 
 ## sklearn logistic regression and random forest classifier 
@@ -33,18 +32,12 @@ from sklearn.tree import DecisionTreeClassifier
 
 set_mpl_params()
 
-## Load data 
-df = pd.read_csv("../data/motion_smartphone.csv")
-df = df.drop("subject", axis=1)
-
-## Convert the activity labels from 1-6 to 0-5
-df.activity -= 1
+# motion data
+X, y = get_activity_data()
 
 ## Generate training and testing set, including all predictor variables  
 x_train, x_test, y_train, y_test =\
-    train_test_split(df.drop(["activity"], axis = 1),
-                     df["activity"], test_size = 0.3,
-                     random_state = 5)
+    train_test_split(X, y, test_size = 0.3, random_state = 5)
 
 ## Load the data into xgb data format
 dtrain = xgb.DMatrix(x_train, label=y_train)
@@ -52,13 +45,14 @@ dtest = xgb.DMatrix(x_test, label=y_test)
 
 ## -------------------------------------------------------------------
 ### Train a xgboost model
-bparam = {'max_depth': 6,                      # max. tree depth
-         'eta': 0.3,                           # learning rate
-         'silent': 1,                          # 0 to print messages
-         'objective': 'multi:softmax',         # multi-class softmax
-         'num_class': 6,                       # classes for softmax
-         'nthread': 4,                         # threads to use
-         'eval_metric': ['merror', 'mlogloss'] # no 'auc' for multi-class
+bparam = {
+    'max_depth': 3,               # max. tree depth
+    'eta': 0.3,                   # learning rate
+    'silent': 1,                  # 0 to print messages
+    'objective': 'multi:softmax', # multi-class softmax
+    'num_class': 6,               # classes for softmax
+    'nthread': 4,                 # threads to use
+    'eval_metric': ['merror', 'mlogloss']
 }
 
 ## Validations set to watch performance
@@ -68,12 +62,24 @@ evals = dict()                  # store validations at each iteration
 # xgboost on a training set
 num_round = 50
 mod = xgb.train(bparam, dtrain, num_round, evals=evallist,
-                evals_result=evals, verbose_eval=False)
+                evals_result=evals, verbose_eval=True,
+                # additional evaluation function to watch
+                feval=xgb_eval_accuracy)
 
-# Show how validation functions have major effect on test performance
+# Comparison of validation metrics
 plot_xgb_evals(evals, n_plot=2, label=True)
 
-    
+## -------------------------------------------------------------------
+### Scikit-learn interface - can't use custom feval the same => gets put into
+# eval_metrics and passed to booster
+clf = xgb.XGBClassifier(objective='mutli:softmax', num_class=6, silent=True,
+                        learning_rate=0.3, random_state=5)
+clf.fit(x_train, y_train)
+print(clf.score(x_test, y_test)) # sklearn mixin for classifier score uses accuracy
+
+# probabilites of points belonging to each class using first 10 boosts
+print(clf.predict_proba(x_test[:1], ntree_limit=10))
+
 ## -------------------------------------------------------------------
 ### Predictions
 preds = mod.predict(dtest)
--- src/xgboost_helpers.py
+++ src/xgboost_helpers.py
@@ -16,6 +16,8 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split, learning_curve
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import roc_curve, auc, precision_recall_curve
+from sklearn.metrics import average_precision_score, roc_auc_score
 from sklearn import model_selection
 
 ## sklearn logistic regression and random forest classifier 
@@ -48,7 +50,152 @@ def set_mpl_params(style='seaborn-whitegrid', **kw):
     plt.style.use(style)    
 
 
-## xgboost on a training set 
+## -------------------------------------------------------------------
+### Example datasets 
+def get_activity_data(data="../data/motion_smartphone.csv"):
+    ## Load data 
+    df = pd.read_csv("../data/motion_smartphone.csv")
+    df = df.drop("subject", axis=1)
+
+    ## Convert the activity labels from 1-6 to 0-5
+    df.activity -= 1
+
+    return df.drop(['activity'], axis=1), df.activity
+
+
+# dataset used in section
+def get_carseats_data():
+    """
+    Get carseats data from ISLR packge in R. Cleans up data for classification
+    by binning the Sales variables and converting factors to numerics.
+
+    Returns transformed carseats (DataFrame, Sales)
+    """
+    url = "https://s3.amazonaws.com/csci-e82-section-data/carseats.csv"
+    carseats = pd.read_csv(url,header=0)
+
+    # Bin 'Sales'
+    #bins = [-1,5.39,9.32,17]
+    #group_names = ['Low', 'Medium', 'High']
+    bins = [-1, 7.49, 17]
+    group_names = ['Low',  'High']
+    carseats['Sales'] = pd.cut(carseats['Sales'], bins, labels=group_names)
+
+    # Freq. count (%) for Sales
+    y = carseats['Sales'].map({'Low': 0, 'High': 1})
+    carseats.drop('Sales', axis=1, inplace=True)
+
+    # Map labels into categorical variables
+    pd.options.mode.chained_assignment = None
+    carseats['ShelveLoc'] = carseats['ShelveLoc']\
+        .map({'Bad': 0, 'Medium': 1, 'Good':2})
+    carseats['Urban'] = carseats['Urban'].map({'No': 0, 'Yes': 1 })
+    carseats['US'] = carseats['US'].map({'No': 0, 'Yes': 1 })
+    pd.options.mode.chained_assignment = 'warn'
+
+    return carseats, y
+
+
+## -------------------------------------------------------------------
+### General classifaction helpers
+
+def plot_confusion_matrix(cm, target_names):
+    """
+    Plot confusion matrix (see section 5 notebook).
+
+    Parameters:
+      - cm (2darray): confusion matrix
+      - target_names (array<str>)
+    """
+    plt.figure(figsize=(5, 5))
+    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.binary)
+    plt.title('Confusion Matrix')
+    plt.set_cmap('Blues')
+    plt.colorbar()
+    tick_marks = np.arange(len(target_names))
+    plt.xticks(tick_marks, target_names, rotation=60)
+    plt.yticks(tick_marks, target_names)
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
+
+
+# From scikit-learn examples -- no longer part of module
+def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
+                        n_jobs=None, train_sizes=np.linspace(.1, 1.0, 5)):
+    """
+    Generate a simple plot of the test and training learning curve.
+    Parameters
+    ----------
+    estimator : object type that implements the "fit" and "predict" methods
+        An object of that type which is cloned for each validation.
+    title : string
+        Title for the chart.
+    X : array-like, shape (n_samples, n_features)
+        Training vector, where n_samples is the number of samples and
+        n_features is the number of features.
+    y : array-like, shape (n_samples) or (n_samples, n_features), optional
+        Target relative to X for classification or regression;
+        None for unsupervised learning.
+    ylim : tuple, shape (ymin, ymax), optional
+        Defines minimum and maximum yvalues plotted.
+    cv : int, cross-validation generator or an iterable, optional
+        Determines the cross-validation splitting strategy.
+        Possible inputs for cv are:
+          - None, to use the default 3-fold cross-validation,
+          - integer, to specify the number of folds.
+          - An object to be used as a cross-validation generator.
+          - An iterable yielding train/test splits.
+        For integer/None inputs, if ``y`` is binary or multiclass,
+        :class:`StratifiedKFold` used. If the estimator is not a classifier
+        or if ``y`` is neither binary nor multiclass, :class:`KFold` is used.
+        Refer :ref:`User Guide <cross_validation>` for the various
+        cross-validators that can be used here.
+    n_jobs : int or None, optional (default=None)
+        Number of jobs to run in parallel.
+        ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
+        ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
+        for more details.
+    train_sizes : array-like, shape (n_ticks,), dtype float or int
+        Relative or absolute numbers of training examples that will be used to
+        generate the learning curve. If the dtype is float, it is regarded as a
+        fraction of the maximum size of the training set (that is determined
+        by the selected validation method), i.e. it has to be within (0, 1].
+        Otherwise it is interpreted as absolute sizes of the training sets.
+        Note that for classification the number of samples usually have to
+        be big enough to contain at least one sample from each class.
+        (default: np.linspace(0.1, 1.0, 5))
+    """
+    plt.figure()
+    plt.title(title)
+    if ylim is not None:
+        plt.ylim(*ylim)
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.grid()
+
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="r")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+             label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+             label="Cross-validation score")
+
+    plt.legend(loc="best")
+    return plt
+
+## -------------------------------------------------------------------
+### XGBoost 
+
 def plot_xgb_evals(evals, n_plot=1, ax=None, show=True, label=True, **kw):
     """
     Plot validation metrics at iterations of XGBoost.
@@ -91,27 +238,6 @@ def plot_xgb_evals(evals, n_plot=1, ax=None, show=True, label=True, **kw):
         plt.show()
     else:
         return ax
-
-
-def plot_confusion_matrix(cm, target_names):
-    """
-    Plot confusion matrix (see section 5 notebook).
-
-    Parameters:
-      - cm (2darray): confusion matrix
-      - target_names (array<str>)
-    """
-    plt.figure(figsize=(5, 5))
-    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.binary)
-    plt.title('Confusion Matrix')
-    plt.set_cmap('Blues')
-    plt.colorbar()
-    tick_marks = np.arange(len(target_names))
-    plt.xticks(tick_marks, target_names, rotation=60)
-    plt.yticks(tick_marks, target_names)
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
-    plt.show()
 
 
 def xgb_metrics(clf, x_test, y_test):
@@ -201,75 +327,32 @@ def xgb_eval_accuracy(preds, actual):
             np.sum(preds == actual.get_label()) / actual.num_row())
 
 
-# From scikit-learn examples -- no longer part of module
-def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
-                        n_jobs=None, train_sizes=np.linspace(.1, 1.0, 5)):
-    """
-    Generate a simple plot of the test and training learning curve.
-    Parameters
-    ----------
-    estimator : object type that implements the "fit" and "predict" methods
-        An object of that type which is cloned for each validation.
-    title : string
-        Title for the chart.
-    X : array-like, shape (n_samples, n_features)
-        Training vector, where n_samples is the number of samples and
-        n_features is the number of features.
-    y : array-like, shape (n_samples) or (n_samples, n_features), optional
-        Target relative to X for classification or regression;
-        None for unsupervised learning.
-    ylim : tuple, shape (ymin, ymax), optional
-        Defines minimum and maximum yvalues plotted.
-    cv : int, cross-validation generator or an iterable, optional
-        Determines the cross-validation splitting strategy.
-        Possible inputs for cv are:
-          - None, to use the default 3-fold cross-validation,
-          - integer, to specify the number of folds.
-          - An object to be used as a cross-validation generator.
-          - An iterable yielding train/test splits.
-        For integer/None inputs, if ``y`` is binary or multiclass,
-        :class:`StratifiedKFold` used. If the estimator is not a classifier
-        or if ``y`` is neither binary nor multiclass, :class:`KFold` is used.
-        Refer :ref:`User Guide <cross_validation>` for the various
-        cross-validators that can be used here.
-    n_jobs : int or None, optional (default=None)
-        Number of jobs to run in parallel.
-        ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
-        ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
-        for more details.
-    train_sizes : array-like, shape (n_ticks,), dtype float or int
-        Relative or absolute numbers of training examples that will be used to
-        generate the learning curve. If the dtype is float, it is regarded as a
-        fraction of the maximum size of the training set (that is determined
-        by the selected validation method), i.e. it has to be within (0, 1].
-        Otherwise it is interpreted as absolute sizes of the training sets.
-        Note that for classification the number of samples usually have to
-        be big enough to contain at least one sample from each class.
-        (default: np.linspace(0.1, 1.0, 5))
-    """
+def plot_xgb_iter_metrics(evals, metric_name='merror'):
+    """Plot validation metrics over iterations (only one metric)."""
+    # mean, std merror / iteration
+    n_iter = len(evals)
+    metrics = np.zeros((n_iter, 4)) # test, train
+    for i, d in evals.items():
+        for j, (dat, met) in enumerate(d.items()):
+            for m, vals in met.items():
+                metrics[i][j*2] = np.mean(vals)
+                metrics[i][j*2+1] = np.std(vals)
+    metrics = pd.DataFrame(metrics, columns=["test-mean", "test-std",
+                                             "train-mean", "train-std"])
+
     plt.figure()
-    plt.title(title)
-    if ylim is not None:
-        plt.ylim(*ylim)
-    plt.xlabel("Training examples")
-    plt.ylabel("Score")
-    train_sizes, train_scores, test_scores = learning_curve(
-        estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
-    train_scores_mean = np.mean(train_scores, axis=1)
-    train_scores_std = np.std(train_scores, axis=1)
-    test_scores_mean = np.mean(test_scores, axis=1)
-    test_scores_std = np.std(test_scores, axis=1)
+    plt.ylabel(metric_name)
+    plt.xlabel("iteration")
+    plt.title("Validation Metrics / Iteration")
     plt.grid()
-
-    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
-                     train_scores_mean + train_scores_std, alpha=0.1,
+    xs = metrics.index.values
+    plt.fill_between(xs, metrics["test-mean"] - metrics["test-std"],
+                     metrics["test-mean"] + metrics["test-std"], alpha=0.1,
                      color="r")
-    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
-                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
-    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
-             label="Training score")
-    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
-             label="Cross-validation score")
-
-    plt.legend(loc="best")
-    return plt
+    plt.fill_between(xs, metrics["train-mean"] - metrics["train-std"],
+                     metrics["train-mean"] + metrics["train-std"], alpha=0.1,
+                     color="g")
+    plt.plot(xs, metrics["test-mean"], 'o-', color="r", label=f"Mean {metric_name}")
+    plt.plot(xs, metrics["train-mean"], 'o-', color="g", label=f"Train {metric_name}")
+    plt.legend(loc='best')
+    plt.show()
